87f463d6ea2438e965839559bab5992b6d3dc96f
Gonçalo Dias <goncalo_dias7@hotmail.com>
2021-05-27 19:52:52 +0100

    Produtos Dinâmicos na Página Inicial

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+// BUSCAR PRODUTOS
+class Produtos {
+  async getProdutos() {
+    try {
+      let resultado = await fetch('produtos.json')
+      let data = await resultado.json();
+      let produtos = data.produtos;
+      produtos = produtos.map(item => {
+        const {}
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+// MOSTRAR PRODUTOS
+class UI {
+
+}
+// LOCAL STORAGE
+class Storage {
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const UI = new UI();
+  const produtos = new Produtos()
+
+  // BUSCAR PRODUTOS
+  produtos.getProdutos().then(data => console.log(data))
+})
+
+// ALTERNAR SECÇÃO DE DETALHES E OPINIÕES
 const seccaoDetalhes = document.getElementById('seccao-detalhes')
 const seccaoOpinioes = document.getElementById('seccao-opinioes')
 const detalhesBtn = document.getElementById('detalhes-btn')
