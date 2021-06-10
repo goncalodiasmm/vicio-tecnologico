370b9e9804953b9e7bd7a13acb0de685ec8c60f1
Gonçalo Dias <goncalo_dias7@hotmail.com>
2021-06-10 13:20:45 +0100

    Estruturação JSON e Formulário de Pagamento

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ class UI {
         <!-- CARTÃO DE PRODUTO -->
            <a href="detalhe-produto.html?id=${produto.id}">
               <div class="flex-v">
-                 <img src=${produto.imagem} alt="${produto.nome}" class="w-16">
+                 <img src=${produto.imagem1} alt="${produto.nome}" class="w-16">
                  <div class="flex-v">
                     <p class="categoria-pequeno">${produto.categoria}</p>
                     <p>${produto.nome}</p>
@@ -87,7 +87,7 @@ class UI {
         <!-- CARTÃO DE PRODUTO -->
            <a href="detalhe-produto.html?id=${produto.id}">
               <div class="flex-v">
-                 <img src=${produto.imagem} alt="${produto.nome}" class="w-16">
+                 <img src=${produto.imagem1} alt="${produto.nome}" class="w-16">
                  <div class="flex-v">
                     <p class="categoria-pequeno">${produto.categoria}</p>
                     <p>${produto.nome}</p>
@@ -116,32 +116,41 @@ class UI {
 
   mostrarProduto(produto) {
     let resultado = `
+    <!-- BREADCRUMBS -->
+    <section id="breadcrumbs">
+      <div class="flex-h alinhar-centro my-2">
+         <i class="ri-home-line"></i>
+         <p class="categoria-pequeno">/ ${produto.categoria}</p>
+         <p class="categoria-pequeno">/ ${produto.subCategoria}</p>
+      </div>
+    </section>
+
     <!-- APRESENTAÇÃO PRODUTO -->
-   <section id="apresentacao-produto">
+    <section id="apresentacao-produto">
       <div class="mt-2 mb-4">
          <div class="flex-h flex-ajustar">
             <!-- FOTOGRAFIAS PRODUTO -->
             <div class="flex-h">
                <div class="flex-v espaço-1">
                   <div class="w-6 border-2 border-solid border-cinzento-10 rounded">
-                     <img src="./img/auriculares-xiaomi-mi-airdots/auriculares-xiaomi-mi-airdots_1.jpg"
-                        alt="auriculares xiaomi">
+                     <img src="${produto.imagem1}"
+                        alt="${produto.nome}">
                   </div>
                   <div class="w-6 border-2 border-solid border-cinzento-10 rounded">
-                     <img src="./img/auriculares-xiaomi-mi-airdots/auriculares-xiaomi-mi-airdots_2.jpg"
-                        alt="auriculares xiaomi">
+                     <img src="${produto.imagem2}"
+                        alt="${produto.nome}">
                   </div>
                   <div class="w-6 border-2 border-solid border-cinzento-10 rounded">
-                     <img src="./img/auriculares-xiaomi-mi-airdots/auriculares-xiaomi-mi-airdots_3.jpg"
-                        alt="auriculares xiaomi">
+                     <img src="${produto.imagem3}"
+                        alt="${produto.nome}">
                   </div>
                   <div class="w-6 border-2 border-solid border-cinzento-10 rounded">
-                     <img src="./img/auriculares-xiaomi-mi-airdots/auriculares-xiaomi-mi-airdots_4.jpg"
-                        alt="auriculares xiaomi">
+                     <img src="${produto.imagem4}"
+                        alt="${produto.nome}">
                   </div>
                </div>
-               <div class="principal">
-                  <img src="${produto.imagem}" alt="">
+               <div>
+                  <img src="${produto.imagem1}" alt="${produto.nome}">
                </div>
             </div>
             <!-- INFORMAÇÃO PRODUTO -->
@@ -151,7 +160,7 @@ class UI {
                      <i class="ri-check-line sucesso"></i>
                      <div>
                         <p class="subtexto negrito">Produto Disponível</p>
-                        <p class="subtexto">53 restantes em stock</p>
+                        <p class="subtexto">${produto.stock} restantes em stock</p>
                      </div>
                   </div>
                   <div class="flex-h">
@@ -166,30 +175,24 @@ class UI {
                <h3>${produto.nome}</h3>
                <div class="flex-h alinhar-centro justificar-entre">
                   <h2>${produto.preco}€</h2>
-                  <h3 class="cruzado cinzento-60 regular">998,99€</h3>
+                  <h3 class="cruzado cinzento-60 regular">${produto.preco}€</h3>
                </div>
                <div class="flex-h alinhar-centro espaço-1">
                   <div class="flex-h alinhar-centro espaço-0-25">
-                     <div class="rating">
-                        <i class="ri-star-fill"></i>
-                        <i class="ri-star-fill"></i>
-                        <i class="ri-star-fill"></i>
-                        <i class="ri-star-fill"></i>
-                        <i class="ri-star-half-fill"></i>
-                     </div>
-                     <p>4.5</p>
+                     <div class="rating">${produto.classificacaoEstrelas}</div>
+                     <p>${produto.classificacaoValor}</p>
                   </div>
                   <div class="divisória-vertical"></div>
                   <div class="flex-h alinhar-centro espaço-0-25">
                      <i class="ri-user-fill"></i>
-                     <p>81 opiniões</p>
+                     <p>${produto.opinioes} opiniões</p>
                   </div>
                </div>
-               <p class="subtexto">ID Produto: 343610</p>
+               <p class="subtexto">ID Produto: ${produto.id}</p>
                <div class="contador-quantidade">
-                  <i class="ri-add-line"></i>
-                  <input type="text" name="quantidade" id="quantidade" value="1">
                   <i class="ri-subtract-line"></i>
+                  <input type="text" name="quantidade" id="quantidade" value="1">
+                  <i class="ri-add-line"></i>
                </div>
                <div class="flex-h alinhar-centro espaço-2">
                   <button onclick="adicionarCarrinho()" class="btn-primário flex-h alinhar-centro espaço-0-25"
@@ -204,7 +207,129 @@ class UI {
             </div>
          </div>
       </div>
-   </section>`
+    </section>
+    
+    <!-- SECÇÃO DETALHES E OPINIÕES -->
+    <section id="detalhes-opiniões">
+      <div class="flex-h justificar-centro mb-4">
+         <button class="borda-primária w-total" id="detalhes-btn">
+            <p class="categoria-médio mb-0-5">Detalhes</p>
+         </button>
+         <button class="borda-cinzento w-total" id="opinioes-btn">
+            <p class="categoria-médio mb-0-5">Opiniões</p>
+         </button>
+      </div>
+      <!-- DETALHES -->
+      <section id="seccao-detalhes">
+        <div class="flex-h flex-1 justificar-centro espaço-4">
+          <div class="flex-v">
+              <p class="subtítulo">${produto.descricaoTitulo}</p>
+              <p>${produto.descricaoCorpo}</p>
+          </div>
+          <div class="flex-v espaço-0-5">
+              <div class="flex-h justificar-entre espaço-8">
+                <p class="negrito">Processador</p>
+                <p>Intel® Core™ i5-10300H 2.5GHz</p>
+              </div>
+              <div class="flex-h justificar-entre">
+                <p class="negrito">Memória</p>
+                <p>16 GB DDR4 2933MHz (2 x 8 GB)</p>
+              </div>
+              <div class="flex-h justificar-entre">
+                <p class="negrito">Armazenamento</p>
+                <p>SSD 512 GB PCIe® NVMe™ M.2</p>
+              </div>
+              <div class="flex-h justificar-entre">
+                <p class="negrito">Áudio</p>
+                <p>Áudio B&O; Dois altifalantes</p>
+              </div>
+              <div class="flex-h justificar-entre">
+                <p class="negrito">Ecrã</p>
+                <p>15.6" FHD (1920 x 1080), 144 Hz</p>
+              </div>
+              <div class="flex-h justificar-entre">
+                <p class="negrito">Gráfica</p>
+                <p>NVIDIA® GeForce RTX™ 2060 Max-Q</p>
+              </div>
+              <div class="flex-h justificar-entre">
+                <p class="negrito">Teclado</p>
+                <p>Numérico, Retroiluminado</p>
+              </div>
+              <div class="flex-h justificar-entre">
+                <p class="negrito">Sistema Operativo</p>
+                <p>Não Incluído (Free DOS)</p>
+              </div>
+          </div>
+        </div>
+      </section>
+      <!-- OPINIÕES -->
+      <section id="seccao-opinioes">
+        <div class="flex-h flex-1 justificar-centro espaço-8 hidden">
+          <div class="flex-v espaço-0-5">
+              <h1>${produto.classificacaoValor}</h1>
+              <div class="rating">${produto.classificacaoEstrelas}</div>
+              <div class="flex-h alinhar-centro">
+                <i class="ri-user-fill cinzento-60"></i>
+                <p>81 opiniões</p>
+              </div>
+              <a href="#" class="btn-secundário categoria-pequeno">Adicionar opinião</a>
+          </div>
+          <!-- COMENTÁRIOS -->
+          <div class="flex-v espaço-2">
+              <!-- COMENTÁRIO -->
+              <div class="flex-h alinhar-inicio espaço-1">
+                <div class="flex-v espaço-0-25">
+                    <p class="categoria-pequeno">João Pereira</p>
+                    <div class="rating">
+                      <i class="ri-star-fill"></i>
+                      <i class="ri-star-fill"></i>
+                      <i class="ri-star-fill"></i>
+                      <i class="ri-star-fill"></i>
+                      <i class="ri-star-half-fill"></i>
+                    </div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt numquam ratione, tenetur nihil,
+                      adipisci quasi odio quidem odit consectetur necessitatibus blanditiis culpa deserunt atque
+                      assumenda cupiditate cumque minima reprehenderit repellendus earum placeat!</p>
+                </div>
+              </div>
+              <!-- COMENTÁRIO -->
+              <div class="flex-h alinhar-inicio espaço-1">
+                <div class="flex-v espaço-0-25">
+                    <p class="categoria-pequeno">João Pereira</p>
+                    <div class="rating">
+                      <i class="ri-star-fill"></i>
+                      <i class="ri-star-fill"></i>
+                      <i class="ri-star-fill"></i>
+                      <i class="ri-star-fill"></i>
+                      <i class="ri-star-half-fill"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi laboriosam illum officiis, id
+                      tenetur nemo sapiente.</p>
+                </div>
+              </div>
+              <!-- COMENTÁRIO -->
+              <div class="flex-h alinhar-inicio espaço-1">
+                <div class="flex-v espaço-0-25">
+                    <p class="categoria-pequeno">João Pereira</p>
+                    <div class="rating">
+                      <i class="ri-star-fill"></i>
+                      <i class="ri-star-fill"></i>
+                      <i class="ri-star-fill"></i>
+                      <i class="ri-star-fill"></i>
+                      <i class="ri-star-half-fill"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur saepe repudiandae explicabo
+                      dolorum delectus consectetur! A, iusto voluptatum qui rem iure ab placeat est porro? Dicta enim
+                      repudiandae fuga tempora! Corporis dicta asperiores et odio tempore facilis eveniet aut iste
+                      corrupti nulla? Est, iusto excepturi! Eaque rerum voluptatem sint voluptate dolor deserunt
+                      accusantium, natus ullam accusamus maiores optio, architecto a libero placeat impedit reiciendis?
+                    </p>
+                </div>
+              </div>
+          </div>
+        </div>
+      </section>
+    </section>`
 
     if (apresentacaoProduto != null) {
       apresentacaoProduto.innerHTML = resultado
@@ -341,6 +466,11 @@ const palavraPasse = document.getElementById('palavra-passe')
 const nome = document.getElementById('nome')
 const apelido = document.getElementById('apelido')
 const confirmarPalavraPasse = document.getElementById('confirmar-palavra-passe')
+const morada = document.getElementById('morada')
+const codPostal = document.getElementById('codigo-postal')
+const pais = document.getElementById('pais')
+const cidade = document.getElementById('cidade')
+const telemovel = document.getElementById('telemovel')
 
 // FORMULÁRIO LOGIN
 if (loginForm != null) {
@@ -421,6 +551,62 @@ if (registoForm != null) {
       e.preventDefault()
     } else {
       definirSucesso(confirmarPalavraPasse)
+    }
+  })
+}
+//FORMULÁRIO DETALHE ENVIO
+
+function carregarPerfil() {
+  nome.value = localStorage.getItem('utilizadorNome', nome.value)
+  apelido.value = localStorage.getItem('utilizadorApelido', apelido.value)
+  email.value = localStorage.getItem('utilizadorEmail', email.value)
+}
+
+if (detalheEnvio != null) {
+  detalheEnvio.addEventListener('submit', (e) => {
+    //validar morada
+    if (morada.value === '') {
+      definirErro(morada, 'Intruduza a morada para envio.')
+      e.preventDefault()
+    } else {
+      definirSucesso(morada, 'A morada é válida.')
+      localStorage.setItem('utilizadorMorada', morada.value)
+    }
+
+    //validar codigo postal
+    // if (codPostal.value === '') {
+    //   definirErro(codPostal, 'Intruduza um codigo válido.')
+    //   e.preventDefault()
+    //} else {
+    //    definirSucesso(morada, 'A morada é válida.')
+    //    localStorage.setItem('utilizadorMorada', morada.value)
+    //}
+
+    //validar país
+
+    if (pais.value === '') {
+      definirErro(pais, 'Intruduza o País para envio.')
+      e.preventDefault()
+    } else {
+      definirSucesso(pais, 'O país é válido.')
+      localStorage.setItem('utilizadorPais', pais.value)
+    }
+
+    //validar cidade
+    if (cidade.value === '') {
+      definirErro(cidade, 'Intruduza a cidade para envio.')
+      e.preventDefault()
+    } else {
+      definirSucesso(pais, 'A cidade é válida.')
+      localStorage.setItem('utilizadoCidade', cidade.value)
+    }
+    //validar NIF
+    if (nif.value === '') {
+      definirErro(nif, 'Intruduza um NIF válido.')
+      e.preventDefault()
+    } else {
+      definirSucesso(pais, 'O NIF é válido.')
+      localStorage.setItem('utilizadorNIF', nif.value)
     }
   })
 }
